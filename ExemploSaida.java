7bc376d38dd68b0543ec4ab79b12f24fb1599b43
Leticia Cristina <133662502+Leticianha@users.noreply.github.com>
2025-02-04 20:53:26 -0300

    Add files via upload

---
+++ ExemploSaida.java
@@ -0,0 +1,8 @@
+public class ExemploSaida {
+	public static void main(String[] args) {
+		System.out.println("Meu nome é Letícia");
+		System.out.println("Meu curso de gradução é Engenharia de Software");
+		System.out.println("Minha idade é: " + 18 + "anos");
+		System.out.println("O resultado é: " + (3 + 5 <= 22));
+	}
+}
