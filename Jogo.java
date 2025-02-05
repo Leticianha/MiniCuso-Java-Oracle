17cef78964eefd62b061a425565dc04eed6a944b
Leticia Cristina <133662502+Leticianha@users.noreply.github.com>
2025-02-05 20:06:45 -0300

    Add files via upload

--- Jogo.java
+++ Jogo.java
@@ -7,7 +7,7 @@ public class Jogo {
     pontos = 1000;
     
       if(pontos >= 1000) {
-        System.out.printIn("Você passou para a fase 2!!!");
+        System.out.println("Você passou para a fase 2!!!");
       }
       
       System.out.println("Próxima instrução.");
