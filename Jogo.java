1517b90d968333321bcda08508218f54fae8b8c3
Leticia Cristina <133662502+Leticianha@users.noreply.github.com>
2025-02-05 19:54:06 -0300

    Add files via upload

---
+++ Jogo.java
@@ -0,0 +1,15 @@
+public class Jogo {
+  
+  public static void main (String[] args) {
+    
+    int pontos;
+    
+    pontos = 1000;
+    
+      if(pontos >= 1000) {
+        System.out.print("Você passou para a fase 2!!!");
+      }
+      
+      System.out.println("Próxima instrução.");
+  }
+}
