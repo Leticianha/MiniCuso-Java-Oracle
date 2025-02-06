e36bdc2eb1fe104abfc4fc67a53fd5fb2268e7e4
Leticia Cristina <133662502+Leticianha@users.noreply.github.com>
2025-02-05 21:05:46 -0300

    Add files via upload

---
+++ Contador.java
@@ -0,0 +1,14 @@
+public class Contador {
+  
+  public static void main (String[] args) {
+     int contador;
+     contador = 1;
+     
+     while(contador <= 100) {
+       System.out.println(contador + "");
+       
+       contador++;
+     }
+  }
+
+}
